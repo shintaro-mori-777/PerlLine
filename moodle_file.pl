ed32b1ec5a7f92122d62d59fd3f4baf2b484e09d
Shintaro Mori <104498859+shintaro-mori-777@users.noreply.github.com>
2022-06-03 23:51:37 +0900

    Add files via upload
    moodleでダウンロードしたファイル名称を学籍番号と氏名に一括して変更する

---
+++ moodle_file.pl
@@ -0,0 +1,62 @@
+
+# ディレクトリの一覧を取得する
+opendir ( DIR, "./" );
+foreach ( readdir ( DIR ) )
+{
+  next if /^\.{1,2}$/;
+  next if /.+\.pl/;
+  push ( @dirname, $_ );
+}
+closedir ( DIR );
+
+# ディレクトリ名称から学籍番号・名前を抽出する
+foreach ( @dirname )
+{
+  @buff = split (/_/, $_ );
+  push ( @fname_new, $buff[0] );
+}
+
+# ディレクトリ内のファイル名を取得する
+foreach $subdir ( @dirname )
+{
+  $subdir = "./".$subdir;
+  opendir ( DIR, $subdir );
+
+  $cnt = 0;
+  foreach ( readdir ( DIR ) )
+  {
+    next if /^\.{1,2}$/;
+    push ( @fname_old, $_ );
+    ++$cnt;
+  }
+  closedir ( DIR );
+
+  if ( $cnt != 1 )
+  {
+    print "Err: ".$subdir;
+  }
+}
+
+# ディレクトリ内のファイル名から拡張子を吸い出す
+foreach ( @fname_old )
+{
+  @buff = split (/\./, $_ );
+  push ( @ftype, $buff[$#buff] );
+}
+
+open ( FILE, ">all_rename.bat" );
+
+$max = @dirname;
+for ( $i = 0; $i < $max; ++$i )
+{
+  $src = "\"".$dirname[$i]."\\".$fname_old[$i]."\"";
+  $dst = "\"".$fname_new[$i].".".$ftype[$i]."\"";
+
+  print FILE "REN\t".$src."\t".$dst."\n";
+}
+
+close ( FILE );
+
+
+
+
