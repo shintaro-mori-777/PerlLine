be4f3472c6f7410f705fe1f30b181e4eca9fef8e
Shintaro Mori <104498859+shintaro-mori-777@users.noreply.github.com>
2022-06-03 23:52:59 +0900

    Add files via upload

---
+++ moodle_file_check.pl
@@ -0,0 +1,78 @@
+
+# ディレクトリの一覧を取得する
+opendir ( DIR, "./" );
+foreach ( readdir ( DIR ) )
+{
+  next if /^\.{1,2}$/;
+  next if /.+\.pl/;
+  next if /.+\.txt/;
+  push ( @dirname, $_ );
+}
+closedir ( DIR );
+
+# ディレクトリ名称から学籍番号・名前を抽出する
+foreach ( @dirname )
+{
+  my @buff = split (/_/, $_ );
+  $buff[0] =~ /(tl\d{6})(.*)/;
+  push ( @fname, $1 );
+}
+
+# ディレクトリ内のファイル名を取得する
+foreach $subdir ( @dirname )
+{
+  my $subdir = "./".$subdir;
+  opendir ( DIR, $subdir );
+
+  $cnt = 0;
+  foreach ( readdir ( DIR ) )
+  {
+    next if /^\.{1,2}$/;
+    push ( @sname, $_ );
+    ++$cnt;
+  }
+  closedir ( DIR );
+
+  if ( $cnt != 1 )
+  {
+    print "Err: ".$subdir;
+  }
+}
+
+# ディレクトリ内のファイル名から拡張子を吸い出す
+foreach ( @sname )
+{
+  @buff = split (/\./, $_ );
+  push ( @ftype, $buff[$#buff] );
+}
+
+# 登録学生リストを読み込む
+open ( FILE, "<list.txt" );
+@reglist = <FILE>;
+close ( FILE );
+foreach ( @reglist )
+{
+  $_ =~ s/\n//g;
+  $_ =~ s/\r\n//g;
+}
+
+# 提出状況
+my %submission = ();
+my %fileex = ();
+
+foreach ( @reglist )
+{
+  $submission{$_} = 'x';
+  $fileex{$_} = '-';
+}
+
+for ( $i = 0; $i < @fname; ++$i )
+{
+  $submission{$fname[$i]} = 'o';
+  $fileex{$fname[$i]} = $ftype[$i];
+}
+
+foreach ( @reglist )
+{
+  print $_."\t".$submission{$_}."\t".$fileex{$_}."\n";
+}
