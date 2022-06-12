67c9f2cfa13a5e59be0b1a57375c323be4277f25
Shintaro Mori <104498859+shintaro-mori-777@users.noreply.github.com>
2022-06-12 22:30:01 +0900

    Update moodle_file.pl

--- moodle_file.pl
+++ moodle_file.pl
@@ -1,3 +1,9 @@
+#  This program code has been implemented by Shintaro MORI
+#    who is assistant professor in Faculty of Engineering,
+#    Fukuoka University, Japan.
+#
+#  Copyright(c) 2022-, Shintaro MORI, All rights reserved.
+
 #! /usr/bin/perl
 
 # ディレクトリの一覧を取得する
@@ -57,7 +63,3 @@ for ( $i = 0; $i < $max; ++$i )
 }
 
 close ( FILE );
-
-
-
-
